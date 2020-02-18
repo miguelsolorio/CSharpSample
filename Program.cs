e6fcd074060af4dac280913cabc73bb36d1626ac
Miguel Solorio <miguel.solorio@microsoft.com>
2020-02-17 21:20:47 -0800

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
